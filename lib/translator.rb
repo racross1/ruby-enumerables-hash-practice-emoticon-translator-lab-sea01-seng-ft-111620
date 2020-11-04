e9332cb1c39c6bf2de7414e03d29270bbb070162
Rachel Cross <racross1@gmail.com>
2020-11-04 18:11:04 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -16,6 +16,8 @@ def get_japanese_emoticon
   # code goes here
 end
 
-def get_english_meaning
-  # code goes here
+def get_english_meaning(path, emoticon)
+  library = load_library(path)
+  
+  
 end
