483d53b0cd5a8b0464d72237aa29a6ef0009a0bb
Rachel Cross <racross1@gmail.com>
2020-11-04 18:30:34 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -22,8 +22,7 @@ def get_english_meaning(path, emoticon)
   library.each do |key, value|
     if value[:japanese] == emoticon
       english_meaning = key
-    else english_meaning = "Sorry
-      binding.pry
+    else english_meaning = "Sorry"
     end 
   end 
   english_meaning
