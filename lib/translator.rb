1937dbb8b11dc04f2eac58d9544addd705f901b7
Rachel Cross <racross1@gmail.com>
2020-11-04 17:38:28 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -5,7 +5,18 @@ require 'pry'
 
 def load_library(path)
 library = YAML.load_file(path)
-final_library = 
+final_library =  library.each_with_object({}) do |(key, value), final_library|
+    value.each do |array_of_emot|
+      
+      
+      names.each do |name|
+        if !final_array[name]
+          final_array[name] = {}
+        end 
+        if !final_array[name][key]
+          !final_array[name][key] = []
+        end 
+        final_array[name][key].push(inner_key.to_s)
 binding.pry
 end
 
